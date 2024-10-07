747c26662d54983037e6fbde6e1e60b500ac28d6
Ge Shi <8281880+geshijoker@users.noreply.github.com>
2024-10-06 20:35:54 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'PoliPrompt'
+copyright = '2024, Menglin Liu & Ge Shi'
+author = 'Menglin Liu & Ge Shi'
 
 release = '0.1'
 version = '0.1.0'
